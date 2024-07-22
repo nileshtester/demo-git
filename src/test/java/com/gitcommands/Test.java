4734037c6ca49b064e024522dc91f033839edc45
nileshtester <nilesh.dhale@kilowott.com>
2024-07-22 15:57:05 +0530

    My first commit

---
+++ src/test/java/com/gitcommands/Test.java
@@ -0,0 +1,21 @@
+package com.gitcommands;
+
+public class Test {
+
+	public static void main(String[] args) {
+		
+		
+		int a=5;
+		int c;
+		
+		for(int i=1;i<=10;i++) {
+			
+			c=a*i;
+			
+			System.out.println(c);
+		}
+		
+
+	}
+
+}
